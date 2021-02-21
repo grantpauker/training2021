582e1a50e4f01eada4ea0ec56abd3b520ed623e2
Grant Pauker <grantpauker@gmail.com>
2021-02-20 21:53:12 -0800

    add turn to angle state machine

--- src/statemachines/turntoangle.py
+++ src/statemachines/turntoangle.py
@@ -1,12 +1,46 @@
-import numpy as np
-import wpilib
 from magicbot.state_machine import StateMachine, state
-from networktables import NetworkTables
-
-from components import chassis, flywheel, turret, vision
-from controls import pidf
-from utils import drivesignal, lazypigeonimu, units
+from components import chassis
+import numpy as np
+from utils import units
 
 
 class TurnToAngle(StateMachine):
-    
+
+    chassis: chassis.Chassis
+
+    KP = 0.25
+    TOLERANCE = 5 * units.degrees
+
+    def __init__(self):
+        self.desired_angle = 90 * units.degrees
+
+    def setup(self):
+        pass
+
+    def align(self):
+        self.engage()
+
+    def isAligned(self):
+        heading = self.chassis.getHeading()
+        return abs(units.angle_diff(heading, self.desired_angle)) <= self.TOLERANCE
+
+    @state(first=True)
+    def turnToAngle(self, initial_call):
+        heading = self.chassis.getHeading()
+        error = units.angle_diff(
+            heading, self.desired_angle
+        )  # number between [-pi, pi]
+        output = self.KP * error  # scale error by proportionality constant
+        self.chassis.setOutput(-output, output)
+        if self.isAligned():
+            self.next_state("lockInPlace")
+
+    @state()
+    def lockInPlace(self, initial_call):
+        self.chassis.stop()
+        if not self.isAligned():
+            self.next_state("turnToAngle")
+
+    def done(self):
+        super().done()
+        self.chassis.stop()
